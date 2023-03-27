a0cdc4e05a968652de01125d962e0468afbc2368
Chris Watson <watzon@pop-os.localdomain>
2023-03-27 15:36:34 -0600

    logo and meta tags

--- src/components/shared/layout_head.cr
+++ src/components/shared/layout_head.cr
@@ -4,11 +4,21 @@ class Shared::LayoutHead < BaseComponent
   def render
     head do
       utf8_charset
-      title @page_title ? "Paste69 - #{@page_title}" : "Paste69"
+      title @page_title ? "Paste69 - #{@page_title}" : "Paste69 - Beautiful open source pastebin service"
       # css_link asset("css/app.css")
       js_link asset("js/app.js"), defer: "true"
       csrf_meta_tags
       responsive_meta_tag
+      meta content: "Paste, save, share, and fork code snippets and other content.", name: "description"
+      meta content: "#1E1E2E", name: "theme-color"
+      meta content: "pastebin,paste,paste tool,code,code snippets,code sharing,code sharing tool,code sharing website,crystal,lucky,luckyframework,crystallang", name: "keywords"
+      meta content: "Paste69 - Beautiful open source pastebin service", property: "og:title"
+      meta content: ENV["APP_DOMAIN"], property: "og:url"
+      meta content: "website", property: "og:type"
+      meta content: "Paste, save, share, and fork code snippets and other content.", name: "og:description"
+      meta content: "Paste69", property: "og:site_name"
+      meta content: "en_US", property: "og:locale"
+      meta content: File.join(ENV["APP_DOMAIN"], asset("images/logo.png")), property: "og:image"
 
       # Used only in development when running `lucky watch`.
       # Will reload browser whenever files change.
