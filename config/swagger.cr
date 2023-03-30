3e032e301d0f9a990645ddfdce81c9af261e88a7
Chris Watson <watzon@pop-os.localdomain>
2023-03-29 22:43:10 -0600

    Update swagger settings

--- config/swagger.cr
+++ config/swagger.cr
@@ -1,7 +1,8 @@
 LuckySwagger.configure do |settings|
-  settings.app_name = "Paste69"
+  settings.title = "Paste69 API"
   settings.version = ENV["APP_VERSION"]
-  settings.uri = "/api/v1"
-  settings.description = "Paste69 pastebin service API"
+  settings.path = "/api/v1"
+  settings.description = "Super simple API for pasting and fetching"
   settings.terms_url = "https://0x45.st/terms"
+  settings.debug_mode = ENV["LUCKY_ENV"] == "development"
 end
