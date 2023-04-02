5118095272eb1671154071d15558baf18f436ae2
Chris Watson <watzon@pop-os.localdomain>
2023-04-01 19:17:56 -0600

    send sentry proper LuckyEnv

--- config/sentry.cr
+++ config/sentry.cr
@@ -3,6 +3,6 @@ shard_yml = YAML.parse({{ read_file("shard.yml") }})
 Raven.configure do |config|
   config.sample_rate = 1.0
   config.async = true
-  config.current_environment = LuckyEnv.name
+  config.current_environment = LuckyEnv.environment
   config.release = shard_yml["version"].as_s?
 end
