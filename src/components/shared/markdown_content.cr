9e36a50c5343903cf61c2211b92bc494133d0527
Chris Watson <cawatson1993@gmail.com>
2023-04-21 16:10:12 -0600

    fix markdown content height

--- src/components/shared/markdown_content.cr
+++ src/components/shared/markdown_content.cr
@@ -1,6 +1,6 @@
 class Shared::MarkdownContent < BaseComponent
   def render
-    div class: "w-full h-full flex-grow bg-[#eff1f5] dark:bg-[#1e1e2e] px-4 py-8" do
+    div class: "w-full min-h-full flex-grow bg-[#eff1f5] dark:bg-[#1e1e2e] px-4 py-8" do
       div class: "prose dark:prose-invert lg:prose-xl" do
         yield
       end
--- src/pages/paste/show_page.cr
+++ src/pages/paste/show_page.cr
@@ -5,7 +5,7 @@ class Paste::ShowPage < MainLayout
     form_for Paste::Create, class: "h-full", autocomplete: "off" do
       div class: "flex flex-col min-h-full justify-between" do
         mount Shared::Navbar, paste: paste
-        div class: "w-full h-full flex-grow" do
+        div class: "w-full min-h-full flex-grow" do
           textarea paste.contents, id: "editor", data_language: paste.language, readonly: true, disabled: true
         end
         mount Shared::Footer
