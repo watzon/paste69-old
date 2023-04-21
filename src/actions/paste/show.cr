1c706417e1a1759a9351ae46fce918747579b3c9
Chris Watson <cawatson1993@gmail.com>
2023-04-21 16:33:46 -0600

    route not found error on missing paste

--- src/actions/paste/show.cr
+++ src/actions/paste/show.cr
@@ -3,21 +3,29 @@ class Paste::Show < BrowserAction
 
   get "/p/:hashed_id" do
     # Check if the id ends with a .md, if so, render it as markdown
-    if hashed_id.ends_with? ".md"
-      id = Hashids.instance.decode(hashed_id[0...-3]).first
-      markdown = true
-    else
-      id = Hashids.instance.decode(hashed_id).first
+    begin
+      if hashed_id.ends_with? ".md"
+        index = hashed_id.rindex(".md")
+        id = Hashids.instance.decode(hashed_id[0...index]).first
+        markdown = true
+      else
+        id = Hashids.instance.decode(hashed_id).first
+      end
+    rescue
+      raise Lucky::RouteNotFoundError.new(context)
     end
 
-    paste = PasteQuery.find(id)
-    if raw
-      plain_text paste.contents
-    elsif markdown
-      raw_html = Luce.to_html(paste.contents, extension_set: Luce::ExtensionSet::GITHUB_WEB)
-      html Paste::Markdown::ShowPage, paste: paste, raw_html: raw_html
+    if paste = PasteQuery.find(id) rescue nil
+      if raw
+        plain_text paste.contents
+      elsif markdown
+        raw_html = Luce.to_html(paste.contents, extension_set: Luce::ExtensionSet::GITHUB_WEB)
+        html Paste::Markdown::ShowPage, paste: paste, raw_html: raw_html
+      else
+        html Paste::ShowPage, paste: paste
+      end
     else
-      html Paste::ShowPage, paste: paste
+      raise Lucky::RouteNotFoundError.new(context)
     end
   end
 end
